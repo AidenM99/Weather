9be816922212f23d16ead8e9947b65651889b908
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-02-03 16:48:57 +0000

    Change API fetch location to prevent errors when searching for cities

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import domFunctions from './dom';
 
-async function getWeatherData(lat, lon) {
+async function getWeatherData(lat, lon, city) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=64dc30bda68554f9c02fcdab98258346`,
@@ -9,7 +9,7 @@ async function getWeatherData(lat, lon) {
 
     const weatherData = await response.json();
 
-    domFunctions.displayWeatherData(weatherData);
+    domFunctions.renderWeatherData(weatherData, city);
   } catch (err) {
     console.log(err);
   }
@@ -18,17 +18,17 @@ async function getWeatherData(lat, lon) {
 export default async function getCity(city) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=64dc30bda68554f9c02fcdab98258346`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=64dc30bda68554f9c02fcdab98258346`,
       { mode: 'cors' }
     );
 
     const cityData = await response.json();
 
-    const x = cityData[0].lat;
+    const x = cityData.coord.lat;
 
-    const y = cityData[0].lon;
+    const y = cityData.coord.lon;
 
-    getWeatherData(x, y);
+    getWeatherData(x, y, city);
   } catch (err) {
     console.log(err);
   }
