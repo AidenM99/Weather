3f5328084964738f0a624ff1ca9f0750f9f3212f
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-02-04 16:07:59 +0000

    Add dynamic rendering of weather data in weather forecast section

--- src/dom.js
+++ src/dom.js
@@ -20,6 +20,48 @@ const domFunctions = (() => {
     });
   }
 
+  function buttonController() {
+    const forecastButtons = document.querySelectorAll('.forecast-button');
+    forecastButtons.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        for (let i = 0; i < 2; i++) {
+          forecastButtons[i].classList.remove('selected-button');
+        }
+        e.target.classList.add('selected-button');
+      });
+    });
+  }
+
+  function renderWeatherForecast(data) {
+    for (let i = 1; i < 8; i++) {
+      const forecastContainer = document.querySelector('.weather-forecast');
+
+      const forecast = document.createElement('div');
+      forecast.classList.add('forecast');
+
+      const day = document.createElement('h3');
+      day.textContent = getDate(data.daily[i].dt, data.timezone_offset).slice(
+        0,
+        3
+      );
+
+      const icon = document.createElement('img');
+      icon.classList.add('weather-icon');
+      icon.src = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`;
+
+      const temp = document.createElement('p');
+      temp.textContent = `${Math.round(data.daily[i].temp.max)}°/${Math.round(
+        data.daily[i].temp.min
+      )}°`;
+
+      forecast.appendChild(day);
+      forecast.appendChild(icon);
+      forecast.appendChild(temp);
+      forecastContainer.appendChild(forecast);
+    }
+    console.log(data);
+  }
+
   function renderWeatherData(data, city) {
     const weatherDesc = document.querySelector('.weather-desc');
     weatherDesc.textContent = formatText(data.current.weather[0].description);
@@ -46,9 +88,21 @@ const domFunctions = (() => {
 
     const windSpeed = document.querySelector('.wind-speed');
     windSpeed.textContent = `Wind: ${Math.round(data.current.wind_speed)} mph`;
+
+    renderWeatherForecast(data);
   }
 
-  return { citySearch, renderWeatherData, reportSearchError };
+  function loadPage() {
+    citySearch();
+    getCity('London');
+    buttonController();
+  }
+
+  return {
+    renderWeatherData,
+    reportSearchError,
+    loadPage,
+  };
 })();
 
 export default domFunctions;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './style.css';
 import domFunctions from './dom';
-import getCity from './api';
 
-window.onload = () => {
-  getCity('London');
-  domFunctions.citySearch();
-};
+domFunctions.loadPage();
